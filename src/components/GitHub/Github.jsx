7d06d2f8187d7969f9ef9ed8d26bcff276da1be1
Pithani Avinash Kumar <avinashkumar.pithani@gmail.com>
2024-08-31 16:27:18 +0530

    fetching data from github api in std way & working with useLoaderData hook

--- src/components/GitHub/Github.jsx
+++ src/components/GitHub/Github.jsx
@@ -1,21 +1,30 @@
 import React, { useEffect, useState } from 'react'
+import { useLoaderData } from 'react-router-dom'
 
 function Github() {
 
-    const [data, setData] = useState([])
-    useEffect(() => {
-        fetch('https://api.github.com/users/AvinashKumarPithani')
-        .then((response) => response.json())
-        .then(data => {
-            console.log(data)
-            setData(data)
-        })
-    }, [])
+    const data = useLoaderData()
+
+    // const [data, setData] = useState([])
+    // useEffect(() => {
+    //     fetch('https://api.github.com/users/ AvinashKumarPithani')
+    //     .then((response) => response.json())
+    //     .then(data => {
+    //         console.log(data)
+    //         setData(data)
+    //     })
+    // }, [])
+
     return (
-        <div className='text-center m-4 bg-gray-600 text-white p-4 text-3xl'>Github Followers: {data.followers}
+        <div className='text-center m-4 bg-gray-600 text-white p-4 text-3xl'>Github Followers: {data.followers ? data.followers : 108}
         <img src={data.avatar_url} width={300} alt="" />
         </div>
     )
 }
 
 export default Github
+
+export const githubInfoLoader = async () => {
+    const response = await fetch('https://api.github.com/users/AvinashKumarPithani')
+    return response.json()
+}
--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import Layout from './Layout.jsx'
 import Home from './components/Home/Home.jsx'
 import About from './components/About/About.jsx'
 import User from './components/User/User.jsx'
-import Github from './components/GitHub/Github.jsx'
+import Github, {githubInfoLoader} from './components/GitHub/Github.jsx'
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -16,7 +16,10 @@ const router = createBrowserRouter(
       <Route path='user/' element={<User />}>
         <Route path=':userid' element={<User />} />
       </Route>
-      <Route path='github' element={<Github />} />
+      <Route 
+      loader={githubInfoLoader} 
+      path='github' 
+      element={<Github />} />
       <Route path='*' element={<div>Not Found</div>} />
     </Route>
   )
